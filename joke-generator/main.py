78ad488bc95641abaae956a687e78e3667c82ef3
Samreen Saif <samreensaif81@gmail.com>
2025-03-17 03:38:25 +0500

    add css

--- joke-generator/main.py
+++ joke-generator/main.py
@@ -16,14 +16,43 @@ def joke_generator():
         return "why u not get error"
 
 def main():
-    st.title("Joke Generator 😂🃏")
-    st.write("Click the button to generate a joke")
+    st.markdown("<h1 style='text-align:center; color:orange; font-size:80px; font-weight:bold; '>Joke Generator <br>😂🃏</h1>", unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align:center; color:green; font-size:20px; font-weight:bold; '>Click the button to generate a joke</h3>", unsafe_allow_html=True)
+        
+
+    st.markdown("""
+    <style>
+    .stButton>button {
+        background-color: #4CAF50; /* Green */
+        color: white;
+        font-size: 20px;
+        font-weight: bold;
+        padding: 10px 20px;
+        border-radius: 10px;
+        cursor: pointer;
+        border: none;
+    }
+    .stButton>button:hover {
+        background-color: black;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+    # Generate Joke Button
     if st.button("Generate Joke"):
         joke = joke_generator()
         st.header(joke)
+    
+    st.markdown("""
+    
+    <div style ="text-align:center; color:red; font-size:20px; font-weight:bold; ">
 
-    st.divider()
-    st.write("Made with ❤️ by [@samreensaif](https://github.com/samreensaif)")
+    <p>Made with ❤️ by <a href="https://github.com/samreensaif">Samreen Saif</a></p>
+
+    </div>
+    
+     """, 
+        unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
